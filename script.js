0f642190e643e799ba3bccc75a3e469daac77433
Shubham Gaurav <123201142+shubhgaurav26@users.noreply.github.com>
2023-06-17 01:37:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector (".sidebar");
+
+menuIcon.oneclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    sidebar.classList
+
+} 
